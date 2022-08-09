90c27567464455da5a5ebdf058ef6c64984d5608
Kári Rafn Karlsson <karirafn@gmail.com>
2022-08-09 11:18:40 +0000

    Added experience and education cv sections

---
+++ src/Web/Models/PeriodEntry.cs
@@ -0,0 +1,21 @@
+﻿namespace Web.Models;
+
+public class PeriodEntry
+{
+    public PeriodEntry(string title, string subtitle, DateOnly startDate, DateOnly? endDate = null, IEnumerable<string>? details = null)
+    {
+        Title = title;
+        Subtitle = subtitle;
+        StartDate = startDate;
+        EndDate = endDate;
+        Details = details ?? Enumerable.Empty<string>();
+    }
+
+    public string Title { get; } = string.Empty;
+    public string Subtitle { get; } = string.Empty;
+    public IEnumerable<string> Details { get; } = Enumerable.Empty<string>();
+    public DateOnly StartDate { get; }
+    public DateOnly? EndDate { get; }
+
+    public string Period => $"{StartDate.Year} - {EndDate?.Year}";
+}
